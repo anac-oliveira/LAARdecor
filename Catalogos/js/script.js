924c9d9a11e9557d20929d76a7275b488a49a7d4
Ana Clara <anac.alves63@aluno.senai.br>
2025-11-26 14:36:25 -0300

    Java Script Finalizado

--- Catalogos/js/script.js
+++ Catalogos/js/script.js
@@ -1,35 +1,36 @@
-// const sombra = document.getElementById("sombra");
-// const busca = document.getElementById("box-busca");
-// const menu = document.getElementById("menu-barras");
+const sombra = document.getElementById("sombra");
+const busca = document.getElementById("box-busca");
+const menu = document.getElementById("menu-barras");
 
-// function mostrarMenu() {
-//     //se
-//     if (window.getComputedStyle(sombra).top != "-210px") {
-//         //se sim
-//         sombra.style.top = "-210px";//escondida
-//         busca.style.top = "-100px";
-//         menu.style.position = "absolute";
-//         busca.style.transition = ".5s";
+function mostrarMenu() {
+    console.log("function");
+    //se
+    if (window.getComputedStyle(sombra).top != "-210px") {
+        //se sim
+        sombra.style.top = "-210px";//escondida
+        busca.style.top = "-100px";
+        menu.style.position = "absolute";
+        busca.style.transition = ".5s";
         
         
-//     } else {/*se não*/
-//         sombra.style.top = "0";//exibido
-//         busca.style.top = "120px";
-//         menu.style.position = "fixed";
-//         busca.style.transition = ".5s";
-//     }
-// }
+    } else {/*se não*/
+        sombra.style.top = "0";//exibido
+        busca.style.top = "120px";
+        menu.style.position = "fixed";
+        busca.style.transition = ".5s";
+    }
+}
 
-// function ocultarMenuResponsivo() {
-//     //se a largura da tela for maior que 768
-//     if (window.innerWidth > 768) {
-//         sombra.style.top = "-210px";
-//         busca.style.top = "0";
+function ocultarMenuResponsivo() {
+    //se a largura da tela for maior que 768
+    if (window.innerWidth > 768) {
+        sombra.style.top = "-210px";
+        busca.style.top = "0";
 
-//     } else {
-//         busca.style.top = "-100px";
-//         busca.style.transition = "0s";
-//     }
-// }
+    } else {
+        busca.style.top = "-100px";
+        busca.style.transition = "0s";
+    }
+}
 
-// window.addEventListener("resize", ocultarMenuResponsivo);
+window.addEventListener("resize", ocultarMenuResponsivo);
